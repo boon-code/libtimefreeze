57dc289f04b79ed9ab31cfc6673f72a0ce57770a
Manuel Huber <manuel.h87@gmail.com>
2022-08-04 01:18:57 +0200

    tests: Refactor frozen time tests
    - Remove duplication
    - Use common test data from `data.h`

---
+++ tests/src/data.h
@@ -0,0 +1,8 @@
+#ifndef _TESTS_DATA_H
+#define _TESTS_DATA_H 1
+
+/* timestamp string and corresponding seconds in UTC */
+#define TEST_TIME_SEC 1641027601
+#define TEST_TIME_STR "2022-01-01 09:00:01"
+
+#endif
