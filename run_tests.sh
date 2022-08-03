89559d18297e888456c22041780c0b9e003b64ac
Manuel Huber <manuel.h87@gmail.com>
2022-08-04 00:11:23 +0200

    Include GoogleTest example in run_tests.sh
    Install GoogleTest in our container image and ensure that the example
    code runs.

--- run_tests.sh
+++ run_tests.sh
@@ -1,5 +1,30 @@
 #!/bin/bash
 
-./build.sh -N -c && \
-    ./run_example.sh && \
-    gcovr . --xml-pretty > coverage.cobertura.xml
+_cmake_build() {
+    if which ninja >/dev/null; then
+        cmake -G Ninja $@ && ninja
+    else
+        cmake $@ && make
+    fi
+}
+
+_run_gtest_example() {
+    ( \
+        cd example/gtest_example/ \
+        && mkdir build/ \
+        && cd build \
+        && _cmake_build ..
+    ) \
+	&& TIMEFREEZE_DEBUG=1 \
+	   LD_PRELOAD=./.build/libtimefreeze.so \
+	   ./example/gtest_example/build/tests
+}
+
+_run_c_example() {
+    ./run_example.sh
+}
+
+./build.sh -N -c \
+    && _run_c_example \
+    && _run_gtest_example \
+    && gcovr . --xml-pretty > coverage.cobertura.xml
